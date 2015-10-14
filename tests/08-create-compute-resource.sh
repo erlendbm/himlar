88aaa7aaed333eb183b2a16621be2d8e9d3790e5
Erlend Midttun <erlend.midttun@ntnu.no>
2015-10-14 09:54:01 +0200

    Add both -controller-01 and -02 as compute resources

--- tests/08-create-compute-resource.sh
+++ tests/08-create-compute-resource.sh
@@ -3,5 +3,7 @@ PREFIX=$(hostname | sed -e 's/-.*$//')
 DOMAIN=$(hostname -f | sed -e 's/^[a-z0-9-]*\.//')
 
 hammer compute-resource list
-hammer compute-resource create --provider Libvirt  --url qemu://${PREFIX}-controller-02.${DOMAIN}/system --name ${PREFIX}-controller-02
+for i in 1 2; do
+  hammer compute-resource create --provider Libvirt  --url qemu+tcp://${PREFIX}-controller-0${i}.${DOMAIN}:16509/system --name ${PREFIX}-controller-0${i}
+done
 hammer compute-resource list
